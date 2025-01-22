b1d496b3c20b2f6592be331576659579120cae7c
CrazyBunQnQ <baobao222222@qq.com>
2025-01-22 23:20:51 +0800

    改用 AI 模型判断图片相似度，能够容忍微小的像素错位及色值偏差，而不是完全一样

--- config.py
+++ config.py
@@ -1,26 +1,28 @@
 # -*- coding:utf-8 -*-
-__author__ = 'Threedog'
-__Date__ = '2018/6/4 11:16'
+__author__ = 'CrazyBunQnQ'
+__Date__ = '2024/11/12 13:14'
 
 # 窗体标题  用于定位游戏窗体
-WINDOW_TITLE = "PictureMatching3"
+WINDOW_TITLE = "BD2"
 # 时间间隔  间隔多少秒连一次
-TIME_INTERVAL = 0.5
+TIME_INTERVAL = 0.3
 # 游戏区域距离顶点的长度
-MARGIN_LEFT = 100
+MARGIN_LEFT = 353
 # 游戏区域距离顶点的高度
-MARGIN_HEIGHT = 100
+MARGIN_HEIGHT = 271
 # 横向的方块数量
-H_NUM = 11
+H_NUM = 16
 # 纵向的方块数量
-V_NUM = 6
+V_NUM = 7
 # 方块宽度
-SQUARE_WIDTH = 65
+SQUARE_WIDTH = 77
 # 方块高度
-SQUARE_HEIGHT = 65
+SQUARE_HEIGHT = 93
 # 切片处理时候的左上、右下坐标：
-# 注意  这里要么保证是21*25，要么，如果不是（比如四个数据是10,10,50,50；也就是40*40像素），name就把empty.png图片替换成对应大小的一张图片（比如40*40），图片可以没用，但程序中不能
-SUB_LT_X = 5
-SUB_LT_Y = 5
-SUB_RB_X = 26
-SUB_RB_Y = 30
+# 注意  这里要么保证裁剪后的像素是21*25，要么（比如四个数据是10,10,50,50；也就是40*40像素）把empty.png图片替换成对应大小的一张图片（比如40*40），图片可以没用，但程序中不能
+SUB_LT_X = 10
+SUB_LT_Y = 11
+SUB_RB_X = 50
+SUB_RB_Y = 71
+# 万恶的 Windows 缩放比例...
+DISPLAY_SCALING = 1.5
--- run.py
+++ run.py
@@ -1,23 +1,57 @@
-'''
-python 版本：3.5
-opencv 下载链接： 
-https://www.lfd.uci.edu/~gohlke/pythonlibs/#opencv
-选择版本：opencv_python‑3.4.1‑cp35‑cp35m‑win_amd64.whl
-pywin32 下载链接：
-https://www.lfd.uci.edu/~gohlke/pythonlibs/#pywin32
-选择版本：pywin32-223-cp35-cp35m-win_amd64.whl 
-'''
-
 import matching
 import cv2
 import numpy as np
+import ctypes
 import win32api
 import win32gui
 import win32con
-from PIL import ImageGrab
+import torch
+from torchvision import models, transforms
+import os
+from PIL import ImageGrab, Image
 import time
 from config import *
+from scipy.spatial.distance import cosine
+import shutil
 
+# 加载预训练的ResNet模型
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = models.resnet18(pretrained=True)
+model = model.to(device)
+model.eval()  # 设为评估模式
+
+# 图像预处理
+preprocess = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+# 提取图像特征
+def extract_features(image_ndarray: np.ndarray):
+    """
+    image_ndarray: 通过OpenCV读取的numpy.ndarray图像(BGR通道)
+    """
+    # BGR -> RGB
+    image_rgb = cv2.cvtColor(image_ndarray, cv2.COLOR_BGR2RGB)
+    # 转成 PIL Image
+    pil_image = Image.fromarray(image_rgb)
+    # 再做transform预处理
+    tensor_image = preprocess(pil_image).unsqueeze(0).to(device)
+    with torch.no_grad():
+        features = model(tensor_image)
+    return features.cpu().numpy().flatten()
+
+# 计算余弦相似度
+def calculate_similarity(features1, features2):
+    return 1 - cosine(features1, features2)
+
+# 判断两张图片是否相似
+def are_images_similar(image1, image2, threshold=0.90):
+    features1 = extract_features(image1)
+    features2 = extract_features(image2)
+    similarity = calculate_similarity(features1, features2)
+    return similarity > threshold
 
 # 获取窗体坐标位置(左上)
 def getGameWindowPosition():
@@ -30,8 +64,12 @@ def getGameWindowPosition():
         window = win32gui.FindWindow(None,WINDOW_TITLE)
     # 定位到游戏窗体
     win32gui.SetForegroundWindow(window) # 将窗体顶置
+    # 将窗体移动到(0,0)
+    # win32gui.MoveWindow(window, 0, 0, width, height, True)
     pos = win32gui.GetWindowRect(window)
     print("定位到游戏窗体：" + str(pos))
+    client_rect = win32gui.GetClientRect(window)
+    print(f"定位到游戏窗体：{pos}, 客户区大小：{client_rect}")
     return (pos[0],pos[1])
 
 # 获取一张完整的屏幕截图
@@ -43,6 +81,9 @@ def getScreenImage():
 
 # 从屏幕截图中识别
 def getAllSquare(screen_image,game_pos):
+    # 确保文件夹存在
+    if not os.path.exists('slice'):
+        os.makedirs('slice')
     print('图像切片处理...')
     # 通过游戏窗体，找到连连看连接的区域：
     game_x = game_pos[0] + MARGIN_LEFT
@@ -54,6 +95,10 @@ def getAllSquare(screen_image,game_pos):
         for y in range(0,V_NUM):
             # ndarray的切片方法，[纵坐标起始位置：纵坐标结束为止，横坐标起始位置：横坐标结束位置]
             square = screen_image[game_y + y * SQUARE_HEIGHT :game_y + (y+1) * SQUARE_HEIGHT,game_x + x * SQUARE_WIDTH:game_x + (x+1) * SQUARE_WIDTH]
+            # 保存每个方块图像到temp文件夹
+            cut_square = square[SUB_LT_Y:SUB_RB_Y, SUB_LT_X:SUB_RB_X]
+            square_filename = f'slice/square_{x}_{y}.png'  # 使用坐标(x, y)作为文件名
+            cv2.imwrite(square_filename, cut_square)  # 保存图像
             all_square.append(square)
     # 因为有些图片的边缘不一致造成干扰（主要是空白区域的切图），所以把每张小方块向内缩小一部分再
     # 对所有的方块进行处理屏蔽掉外边缘 然后返回
@@ -67,17 +112,26 @@ def getAllSquare(screen_image,game_pos):
     # return new_all_square
 
 # 判断图像是否与已经在列表中的图像相同，如果是返回True
-def isImageExist(img,img_list):
+def isImageExist(img,img_list, threshold=0.80):
     for existed_img in img_list:
-        b = np.subtract(existed_img,img) # 图片数组进行比较，返回的是两个图片像素点差值的数组，
-        if not np.any(b):   # 如果全部是0，说明两图片完全相同。
+        # 每组方块图像完全相同时使用此代码
+        # b = np.subtract(existed_img,img) # 图片数组进行比较，返回的是两个图片像素点差值的数组，
+        # if not np.any(b):   # 如果全部是0，说明两图片完全相同。
+        #     return True
+        # else:
+        #     continue
+        # 有些情况下，每组图片有微小的差异，色值或错位等，这时使用相似度判断，可以容忍微笑的错位及色值变化
+        if are_images_similar(existed_img, img):
             return True
-        else:
-            continue
     return False
 
 # 获取所有的方块类型
 def getAllSquareTypes(all_square):
+    # 如果文件夹存在，则删除并重新创建
+    if os.path.exists('temp'):
+        shutil.rmtree('temp')  # 删除文件夹及其中所有内容
+    os.makedirs('temp')  # 重新创建文件夹
+    n = 1
     print("将图像矩阵按类型归入类型列表...")
     types = []
     # 先把空白添加到数组中，作为0号
@@ -86,6 +140,10 @@ def getAllSquareTypes(all_square):
     for square in all_square:
         # 如果这个图像不存在的话将图像保存起来
         if not isImageExist(square,types):
+            # 保存每个方块图像到temp文件夹
+            square_filename = f'temp/square_{n}.png'  # 使用坐标(x, y)作为文件名
+            cv2.imwrite(square_filename, square)  # 保存图像
+            n = n + 1
             types.append(square)
     return types
 
@@ -97,8 +155,11 @@ def getAllSquareRecord(all_square_list,types):
     for square in all_square_list:   # 把所有的方块和保存起来的所有类型做对比
         num = 0
         for type in types:    # 所有类型
-            res = cv2.subtract(square,type) # 作比较
-            if not np.any(res):     # 如果两个图片一样
+            # 判断完全一样
+            # res = cv2.subtract(square,type) # 作比较
+            # if not np.any(res):     # 如果两个图片一样
+            # 使用模糊判断是否匹配，能够容忍微笑的错位及颜色变化
+            if are_images_similar(square, type):
                 line.append(num)    # 将类型的数字记录进这一行
                 break               # 并且跳出循环
             num += 1                # 如果没有匹配上，则类型数加1
@@ -123,20 +184,24 @@ def autoRelease(result,game_x,game_y):
                             # 执行消除算法并返回
                                 result[i][j] = 0
                                 result[m][n] = 0
-                                print('可消除点：'+ str(i+1) + ',' + str(j+1) + '和' + str(m+1) + ',' + str(n+1))
                                 x1 = game_x + j*SQUARE_WIDTH
                                 y1 = game_y + i*SQUARE_HEIGHT
                                 x2 = game_x + n*SQUARE_WIDTH
                                 y2 = game_y + m*SQUARE_HEIGHT
-                                win32api.SetCursorPos((x1 + 15,y1 + 18))
-                                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x1+15, y1+18, 0, 0)
-                                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x1+15, y1+18, 0, 0)
+                                click1X = int((x1 + 15)/DISPLAY_SCALING)
+                                click2Y = int((y1 + 18)/DISPLAY_SCALING)
+                                win32api.SetCursorPos((click1X,click2Y))
+                                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, click1X, click2Y, 0, 0)
+                                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, click1X, click2Y, 0, 0)
                                 time.sleep(TIME_INTERVAL)
 
-                                win32api.SetCursorPos((x2 + 15, y2 + 18))
-                                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x2 + 15, y2 + 18, 0, 0)
-                                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x2 + 15, y2 + 18, 0, 0)
+                                click2X = int((x2 + 15)/DISPLAY_SCALING)
+                                click2Y = int((y2 + 18)/DISPLAY_SCALING)
+                                win32api.SetCursorPos((click2X, click2Y))
+                                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, click2X, click2Y, 0, 0)
+                                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, click2X, click2Y, 0, 0)
                                 time.sleep(TIME_INTERVAL)
+                                print('可消除点：'+ str(i+1) + ',' + str(j+1) + '(' + str(click1X) + ',' + str(click2Y) +')和' + str(m+1) + ',' + str(n+1) + '(' + str(click2X) + ',' + str(click2Y) + ')')
                                 return True
     return False
 
